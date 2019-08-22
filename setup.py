0129469f8dcdc47facbb537a3965cebe399946d1
vitche <developers@vitche.com>
2019-08-22 18:05:55 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup (
    description = 'Compute graph implementation for Python.',
    author = 'Vitche Research Team Developer',
    author_email = 'developer@vitche.com',
-   py_modules = ['compute.graph', 'compute.graph.profile', 'compute.graph.activation', 'compute.graph.visualization'],
+   py_modules = ['compute.graph', 'compute.graph.structure', 'compute.graph.profile', 'compute.graph.activation', 'compute.graph.visualization'],
    install_requires = []
 )
