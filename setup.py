d43c2de71c77b4e836111f5acb0867efebaf2b63
vitche <developers@vitche.com>
2019-08-22 17:52:32 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup (
+   name = 'compute_graph',
+   version = '1.0',
+   description = 'Compute graph implementation for Python.',
+   author = 'Vitche Research Team Developer',
+   author_email = 'developer@vitche.com',
+   py_modules = ['compute.graph', 'compute.graph.profile', 'compute.graph.activation', 'compute.graph.visualization'],
+   install_requires = []
+)
