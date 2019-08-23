d6d0980b1c9fbf8a4e8a92c099f4f1875427edd6
vitche <developers@vitche.com>
2019-08-23 12:17:06 +0300

    Update profile.py

--- compute/graph/profile.py
+++ compute/graph/profile.py
@@ -1,1 +1,10 @@
+class ProfileGraph(Graph):
+    def add(self, node):
+        node = super().add(node)
+        node.add(ProfilerNode())
+        return node
 
+class ProfilerNode(Node):
+    def write(self, data):
+        if None != data:
+            self.arguments(data)
