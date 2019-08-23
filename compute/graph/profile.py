b6983eae826253e2d911f556bc464a0fc62add1b
vitche <developers@vitche.com>
2019-08-23 15:11:45 +0300

    Update profile.py

--- compute/graph/profile.py
+++ compute/graph/profile.py
@@ -9,6 +9,16 @@ class ProfileGraph(Graph):
         return node
 
 class ProfileNode(Node):
+    def __init__(self):
+        super().__init__()
+        self.delta = {
+            "amount": 0,
+            "time": 0
+        }
     def write(self, data):
         if None != data:
-            self.arguments(data)
+            for item in data:
+                if 'amount' in item and 'time' in item:
+                    self.delta["amount"] += item["amount"]
+                    self.delta["time"] += item["time"]
+        self.arguments([self.delta])
