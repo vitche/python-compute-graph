2fdac6d6b165dcbcf6eb0d8c9e425e34139b16f3
pysyun <48061047+pysyun@users.noreply.github.com>
2019-08-23 13:43:46 +0300

    Update profile.py

--- compute/graph/profile.py
+++ compute/graph/profile.py
@@ -1,10 +1,11 @@
 from compute.graph.structure import Graph
 from compute.graph.structure import Node
+from compute.graph.structure import Synapse
 
 class ProfileGraph(Graph):
     def add(self, node):
         node = super().add(node)
-        node.add(ProfileNode())
+        node.add(Synapse().add(ProfileNode()))
         return node
 
 class ProfileNode(Node):
