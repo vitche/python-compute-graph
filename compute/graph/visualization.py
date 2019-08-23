54f6b76eb37dfc95f999c4b963542f3f51a2ff06
vitche <developers@vitche.com>
2019-08-23 12:30:23 +0300

    Update visualization.py

--- compute/graph/visualization.py
+++ compute/graph/visualization.py
@@ -1,1 +1,36 @@
+from graphviz import Digraph
+from graphviz import Source
 
+import graphviz
+from IPython.display import display
+
+class GraphvizNode(Node):
+
+    def __traverse(self, graph, node):
+
+        synapses = node.synapses()
+
+        # Extract deltas from profiler nodes if any
+        delta = None
+        for i in range(len(synapses)):
+            neighborNode = synapses[i].node()
+            if isinstance(neighborNode, ProfileNode):
+                deltas = neighborNode.read()
+                if 0 < len(deltas):
+                    delta = deltas[0]
+
+        for i in range(len(synapses)):
+            neighborNode = synapses[i].node()
+            if isinstance(neighborNode, ProfileNode):
+                continue
+            graph.node(str(neighborNode))
+            self.__traverse(graph, neighborNode)
+            label = ""
+            if None != delta:
+                label = str(delta["amount"]) + " (N), " + str(delta["time"]) + " (s)"
+            graph.edge(str(node), str(neighborNode), label = label)
+
+    def process(self):
+        dot = Digraph()
+        self.__traverse(dot, self)
+        display(Source(dot))
